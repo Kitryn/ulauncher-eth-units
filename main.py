8c10e2d6d364928bdfd0b1144d12ffb135f0200e
Yixun Chen <yixun@alphalab.capital>
2022-09-14 12:11:28 +0800

    initial commit

---
+++ main.py
@@ -0,0 +1,63 @@
+from ulauncher.api.client.Extension import Extension
+from ulauncher.api.client.EventListener import EventListener
+from ulauncher.api.shared.action.CopyToClipboardAction import CopyToClipboardAction
+from ulauncher.api.shared.event import KeywordQueryEvent
+from ulauncher.api.shared.item.ExtensionResultItem import ExtensionResultItem
+from ulauncher.api.shared.action.RenderResultListAction import RenderResultListAction
+
+
+class EthToolsExtension(Extension):
+    def __init__(self):
+        super().__init__()
+        self.subscribe(KeywordQueryEvent, KeywordQueryEventListener())
+
+
+def convert(vtype, val):
+    if vtype == "wei":
+        return val
+    elif vtype == "gwei":
+        return int(val * 1e9)
+    elif vtype == "ether":
+        return int(val * 1e18)
+
+
+def format_results(val):
+    return [
+        ExtensionResultItem(
+            name=str(val),
+            description="wei",
+            on_enter=CopyToClipboardAction(str(val)),
+            highlightable=False
+        ),
+        ExtensionResultItem(
+            name=str(val / 1e9),
+            description="gwei",
+            on_enter=CopyToClipboardAction(str(val / 1e9)),
+            highlightable=False
+        ),
+        ExtensionResultItem(
+            name=str(val / 1e18),
+            description="ether",
+            on_enter=CopyToClipboardAction(str(val / 1e18)),
+            highlightable=False
+        )
+    ]
+
+
+class KeywordQueryEventListener(EventListener):
+    def on_event(self, event, extension):
+        argument = (event.get_argument() or '').encode('utf-8')
+        keyword = event.get_keyword()
+
+        keyword_id = keyword
+        for kwId, kw in extension.preferences.items():
+            if kw == keyword:
+                keyword_id = kwId
+
+        val = convert(keyword_id, float(argument))
+
+        return RenderResultListAction(format_results(val))
+
+
+if __name__ == '__main__':
+    EthToolsExtension().run()
